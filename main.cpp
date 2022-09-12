a2462ed67eccaa9984f87e015ae1430f05267248
hemanthtshemanth <113166352+hemanthtshemanth@users.noreply.github.com>
2022-09-12 18:06:00 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
-
+#define led_pin 13;
+#define in_pin 7;
+int var;
 void setup() {
+  pinMode(led_pin,OUTPUT);
+  pinMode(in_pin,INPUT);
   // put your setup code here, to run once:
 }
 
 void loop() {
+var=digitalRead(in_pin);
+digitalWrite(led_pin,var);
   // put your main code here, to run repeatedly:
 }
